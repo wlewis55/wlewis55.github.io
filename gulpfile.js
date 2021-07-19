f5a7c3dd3bdcd5a638070c82c1d8ec3de2a33232
Wade Lewis <wlewis830@gmail.com>
2021-07-18 23:00:57 -0400

    Stock images, glightbox fix, now uses scss

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync').create();
+
+const { series } = require('gulp');
+
+
+//compile scss into css
+function style() {
+    return gulp.src('assets/scss/*.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.stream());
+}
+function watch() {
+    browserSync.init({
+        server: {
+           baseDir: "./",
+           index: "index.html"
+        }
+    });
+    gulp.watch('assets/scss/*.scss', style)
+    gulp.watch(['*.html','portfolio/*.html']).on('change',browserSync.reload);
+    gulp.watch(['assets/js/*.js', 'assets/vendor/**/*.js']).on('change', browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
+
+exports.default = series(style, watch);
